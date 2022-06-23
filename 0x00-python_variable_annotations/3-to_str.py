714ca4279d2dad8978a09b04dbc2a93d1400c105
pericles001 <periclesadjovi@outlook.fr>
2022-06-23 16:54:46 +0100

    Edit file with str method

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    takes a float and returns the string representation of the float
+    :param n:
+    :return:
+    """
+    return str(n)
