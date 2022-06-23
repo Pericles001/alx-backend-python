8a04ead3ad1bca79d8eecfb785e99acd2a52dccf
pericles001 <periclesadjovi@outlook.fr>
2022-06-23 17:07:06 +0100

    Review file

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,11 +4,10 @@ Complex types - list of floats
 """
 from typing import List
 
+
 def sum_list(input_list: list[float]) -> float:
     """
     takes a list input_list of floats as
     argument and returns their sum as a float
-    :param input_list:
-    :return:
     """
     return sum(input_list)
