54ae7bde8a927f765a8df137e64b8d9d5bd50fe7
pericles001 <periclesadjovi@outlook.fr>
2022-06-23 17:04:05 +0100

    Edit file with sum_list method

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Complex types - list of floats
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    takes a list input_list of floats as
+    argument and returns their sum as a float
+    :param input_list:
+    :return:
+    """
+    return sum(input_list)
