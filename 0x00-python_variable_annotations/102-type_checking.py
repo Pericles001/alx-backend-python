53b67177fd33e292f32bf642d3f4fa7a97e2c998
pericles001 <periclesadjovi@outlook.fr>
2022-06-23 18:09:52 +0100

    Update file

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,6 +7,10 @@ from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    return a list of tuples with the 
+    lements of lst zoomed by factor
+    """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
@@ -15,7 +19,5 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
 
 
 array = [12, 72, 91]
-
 zoom_2x = zoom_array(tuple(array))
-
 zoom_3x = zoom_array(tuple(array), int(3.0))
