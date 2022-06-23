b922760758b91db24794833e26b2629f044bdda0
pericles001 <periclesadjovi@outlook.fr>
2022-06-23 16:23:46 +0100

    Review file

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,13 +3,10 @@
 Basic annotations - add
 """
 
+
 def add(a: float, b: float) -> float:
     """
-    takes a float a and a float b as arguments
-     and returns their sum as a float.
-
-    :param a:
-    :param b:
-    :return:
+    takes arguments and returns their
+     sum as a float.
     """
     return a + b
