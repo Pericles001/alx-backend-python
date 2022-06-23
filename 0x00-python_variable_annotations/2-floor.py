9b07731d9e6b34d5fd5f1249275b6120d13f626a
pericles001 <periclesadjovi@outlook.fr>
2022-06-23 16:43:13 +0100

    Edit file with floor method

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+"""
+
+
+def floor(n: float) -> float:
+    """
+    takes a float and returns the floor of the float
+    :param n:
+    :return:
+    """
+    return n // 1
