bebb64cff2b7531acf4c4002129e3de8f3bc3c06
pericles001 <periclesadjovi@outlook.fr>
2022-06-23 16:18:03 +0100

    Edit file -Create method add -Fix pycodestyle error

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - add
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    takes a float a and a float b as arguments
+     and returns their sum as a float.
+
+    :param a:
+    :param b:
+    :return:
+    """
+    return a + b
