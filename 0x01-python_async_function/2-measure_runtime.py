f5ae77cc55d51ab6a56786bf090b443dba7e714f
pericles001 <periclesadjovi@outlook.fr>
2022-06-27 21:02:12 +0100

    Update file -Remove end variable

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -19,6 +19,5 @@ def measure_time(n: int, max_delay: int) -> float:
     """
     start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    end = time.perf_counter()
-    total_time = end - start
+    total_time = time.perf_counter() - start
     return total_time / n
