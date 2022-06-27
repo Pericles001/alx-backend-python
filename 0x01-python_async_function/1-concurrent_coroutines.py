3ae40e11032d83a006bbac2a66d9b7d06c1cf562
pericles001 <periclesadjovi@outlook.fr>
2022-06-27 20:25:49 +0100

    Update file -Fix typo at line 13

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,14 +10,10 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     return the list of all the
      delays (float values).
-      The list of the delays
-      should be in ascending order
-      without using sort() because
-      of concurrency.
     """
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
