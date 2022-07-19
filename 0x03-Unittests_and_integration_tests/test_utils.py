4d1cf5512aba6943f2124cf44f6f8ef6da7975c7
Pericles001 <periclesadjovi@gmail.com>
2022-07-19 03:45:04 +0100

    Update file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -64,25 +64,25 @@ class TestGetJson(unittest.TestCase):
             MockClass.return_value = Mocked()
             self.assertEqual(get_json(url), payload)
 
-    class TestMemoize(unittest.TestCase):
-        """
-        memoize test class
-        """
-        def test_memoize():
-            """
-            memoize test method
-            """
-            class TestClass:
 
-                def a_method(self):
-                    return 42
+class TestMemoize(unittest.TestCase):
+    ''' memoize unittest '''
 
-                @memoize
-                def a_property(self):
-                    return self.a_method()
+    def test_memoize(self):
+        ''' memoize test '''
 
-            with patch.object(TestClass, 'a_method') as mocked:
-                spec = TestClass()
-                spec.a_property
-                spec.a_property
-                mocked.assert_called_once()
+        class TestClass:
+            ''' self descriptive'''
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mocked:
+            spec = TestClass()
+            spec.a_property
+            spec.a_property
+            mocked.asset_called_once()
