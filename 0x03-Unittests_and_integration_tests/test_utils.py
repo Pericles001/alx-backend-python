93be608c20e95f20aec98890032ba0a2223d6028
Pericles001 <periclesadjovi@gmail.com>
2022-07-19 03:03:05 +0100

    Edit file with expand parameter

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,3 +22,15 @@ class TestAccessNestedMap(unittest.TestCase):
         method to test that the method returns what it is supposed to.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+         test that a KeyError is raised for
+         the following inputs (use @parameterized.expand)
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
