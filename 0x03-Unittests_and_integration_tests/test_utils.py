0127a6e6421e9e12320a60238750283198cdfb86
Pericles001 <periclesadjovi@gmail.com>
2022-07-19 02:40:23 +0100

    Refactor code

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,18 +8,17 @@ from unittest.mock import patch, Mock
 from utils import access_nested_map, get_json, memoize
 
 
-class TestAccessNestedMap(access_nested_map):
+class TestAccessNestedMap(unittest.TestCase):
     """
     class that inherits from unittest.TestCase
     """
-    
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map:
+    def test_access_nested_map(self, nested_map, path, expected):
         """
         method to test that the method returns what it is supposed to.
         """
-        
+        self.assertEqual(access_nested_map(nested_map, path), expected)
