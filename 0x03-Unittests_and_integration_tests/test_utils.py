df926acaa39d2dd5fdb69e5723f24ddef6cd656e
Pericles001 <periclesadjovi@gmail.com>
2022-07-19 03:26:21 +0100

    Edit file with mock http call test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -34,3 +34,31 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+     implement the TestGetJson.test_get_json
+     method to test that utils.get_json returns the expected result.
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url, payload):
+        """
+        method to test that the method returns what it is supposed to.
+        """
+        class Mocked(Mock):
+            """
+            class that inherits from Mock
+            """
+            def json(self):
+                """
+                json returning a payload
+                """
+                return payload
+
+        with patch("requests.get") as MockClass:
+            MockClass.return_value = Mocked()
+            self.assertEqual(get_json(url), payload)
